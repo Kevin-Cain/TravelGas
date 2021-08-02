b4ce1fb6c0f8da5c940327fbfa295cfc7321c151
Kevin-Cain <kevin.m.cain2@gmail.com>
2021-08-01 22:47:56 -0700

    db commit 2

--- website/models.py
+++ website/models.py
@@ -4,6 +4,8 @@ from sqlalchemy.sql import func
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(150), unique=True)
@@ -13,7 +15,46 @@ class User(db.Model, UserMixin):
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
-    MPG = db.Column(db.Integer())
-    GasTankCapacity = db.Column(db.Integer())
+    mpg = db.Column(db.Integer())
     HomeAddress = db.Column(db.String(150))
     Destination = db.Column(db.String(150))
+    fuel = db.Column(db.Integer())
+    time = db.Column(db.String(150))
+    distance = db.Column(db.Float())
+    cost = db.Column(db.Float())
+
+    mondayMaxTemp = db.Column(db.Integer())
+    mondayMinTemp = db.Column(db.Integer())
+    mondayIcon = db.Column(db.String(150))
+    mondayDescription = db.Column(db.String(150))
+
+    tuesdayMaxTemp = db.Column(db.Integer())
+    tuesdayMinTemp = db.Column(db.Integer())
+    tuesdayIcon = db.Column(db.String(150))
+    tuesdayDescription = db.Column(db.String(150))
+
+    wensdayMaxTemp = db.Column(db.Integer())
+    wensdayMinTemp = db.Column(db.Integer())
+    wensdayIcon = db.Column(db.String(150))
+    wensdayDescription = db.Column(db.String(150))
+
+    thursdayMaxTemp = db.Column(db.Integer())
+    thursdayMinTemp = db.Column(db.Integer())
+    thursdayIcon = db.Column(db.String(150))
+    thursdayDescription = db.Column(db.String(150))
+
+    fridayMaxTemp = db.Column(db.Integer())
+    fridayMinTemp = db.Column(db.Integer())
+    fridayIcon = db.Column(db.String(150))
+    fridayDescription = db.Column(db.String(150))
+
+    saturdayMaxTemp = db.Column(db.Integer())
+    saturdayMinTemp = db.Column(db.Integer())
+    saturdayIcon = db.Column(db.String(150))
+    saturdayDescription = db.Column(db.String(150))
+
+    sundayMaxTemp = db.Column(db.Integer())
+    sundayMinTemp = db.Column(db.Integer())
+    sundayIcon = db.Column(db.String(150))
+    sundayDescription = db.Column(db.String(150))
+
